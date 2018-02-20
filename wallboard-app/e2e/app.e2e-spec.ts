c3ae51a82482ea5e2072eb5e0565a9199b945ac6
Yannick Kirschhoffer <yannick.kirschhoffer@luxairgroup.lu>
2018-02-20 16:24:59 +0100

    Update the Karma tests to match the latest application content.

--- wallboard-app/e2e/app.e2e-spec.ts
+++ wallboard-app/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import {AppPage} from './app.po';
 
-describe('wallboard-app App', () => {
+describe('Voxxed Days Luxembourg Wallboard App', () => {
     let page: AppPage;
 
     beforeEach(() => {
         page = new AppPage();
     });
 
-    it('should display welcome message', () => {
+    it('should display current conference', () => {
         page.navigateTo();
-        expect(page.getParagraphText()).toEqual('Welcome to app!');
+        expect(page.getParagraphText()).toContain('Current Talk');
     });
 });
