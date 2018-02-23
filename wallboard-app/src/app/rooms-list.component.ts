2d953ae056ac4e6479f1c8f20ed3928169ddf192
Yannick Kirschhoffer <alcibiade@alcibiade.org>
2018-02-23 18:28:59 +0100

    Extract rooms list from slots information.

--- wallboard-app/src/app/rooms-list.component.ts
+++ wallboard-app/src/app/rooms-list.component.ts
@@ -1,18 +1,26 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Slot} from "./schedule";
+import {Component, Input, OnChanges} from '@angular/core';
+import {Room, Slot} from "./schedule";
 
 @Component({
     selector: 'app-rooms-list',
     templateUrl: './rooms-list.component.html',
     styleUrls: ['./rooms-list.component.css']
 })
-export class RoomsListComponent implements OnInit {
+export class RoomsListComponent implements OnChanges {
     @Input() slots: Slot[];
+    rooms: Set<Room>;
 
     constructor() {
     }
 
-    ngOnInit() {
-    }
+    ngOnChanges() {
+        let rooms = new Map();
 
+        for (let slot of this.slots) {
+            rooms.set(slot.roomId, new Room(slot.roomId, slot.roomName));
+        }
+
+        console.log('Rooms are now', rooms);
+        this.rooms = new Set(rooms.values());
+    }
 }
--- wallboard-app/src/app/schedule.ts
+++ wallboard-app/src/app/schedule.ts
@@ -1,7 +1,14 @@
 export class Slot {
-
+    roomId: string;
+    roomName: string;
 }
 
 export class Schedule {
-    slots: Slot[]
+    slots: Slot[];
+}
+
+export class Room {
+    constructor(public roomId: string,
+                public roomName: string) {
+    }
 }
