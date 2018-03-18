0234ddacb4d49110ab3030b5e002f71a12996c35
Yannick Kirschhoffer <alcibiade@alcibiade.org>
2018-03-18 20:59:05 +0100

    Fix URLs to use our different test/production instances.

--- wallboard-app/src/environments/environment.prod.ts
+++ wallboard-app/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
+// noinspection JSUnusedGlobalSymbols
 export const environment = {
     production: true,
     fullScheduleUrl: 'https://cfp-voxxed-lux.yajug.org/api/conferences/voxxeddaylux2017/schedules/friday',
-    speakersUrl: 'view-source:https://cfp-voxxed-lux.yajug.org/api/conferences/voxxeddaylux2017/speakers'
+    speakersUrl: 'https://cfp-voxxed-lux.yajug.org/api/conferences/voxxeddaylux2017/speakers'
 };
--- wallboard-app/src/environments/environment.ts
+++ wallboard-app/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    fullScheduleUrl: 'https://cfp-voxxed-lux.yajug.org/api/conferences/voxxeddaylux2017/schedules/friday',
-    speakersUrl: 'view-source:https://cfp-voxxed-lux.yajug.org/api/conferences/voxxeddaylux2017/speakers'
+    fullScheduleUrl: 'https://vdlu-2018-test.cleverapps.io/api/conferences/voxxeddaylux2018/schedules/friday',
+    speakersUrl: 'https://vdlu-2018-test.cleverapps.io/api/conferences/voxxeddaylux2018/speakers'
 };
 
